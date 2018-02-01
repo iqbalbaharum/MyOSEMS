12251f234fa8096866f7328c8352819bbbb275a1
iqbalbaharum <balainkk@gmail.com>
2018-02-01 17:32:09 +0800

    update file

--- app/routes.js
+++ app/routes.js
@@ -16,3 +16,11 @@ router.get("/meetup", (req, res) => {
 router.get("/register", (req, res) => {
   res.render('register');
 });
+
+router.get("/login", (req, res) => {
+  res.render('login');
+});
+
+router.get("/register", (req, res) => {
+  res.render('register');
+});
